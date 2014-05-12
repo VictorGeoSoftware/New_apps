33497ab18ec8b36f8f7facd631f4bca6d80bfdb7
VictorGeoSoftware <vpalmacarrasco@gmail.com>
2014-05-12 18:08:25 +0200

    Side toobar implementada.

--- Cartelera/src/com/victor/cartelera/MainActivity.java
+++ Cartelera/src/com/victor/cartelera/MainActivity.java
@@ -1,7 +1,15 @@
 package com.victor.cartelera;
 
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.ProgressDialog;
+import android.os.AsyncTask;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
+import android.support.v4.app.ActionBarDrawerToggle;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -10,13 +18,32 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.LinearLayout;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends ActionBarActivity 
 {
 	//----- VIEW ELEMENTS
+	//----- Sidebar
+    private DrawerLayout mDrawerLayout;
+    private ListView mDrawerList;
+    private ActionBarDrawerToggle mDrawerToggle;
+	
+	
+	
+	
+	//----- VARIABLES
+		//----- Sidebar
+	private CharSequence mTitle;
+	private CharSequence mDrawerTitle;
+	private String[] mPlanetTitles;
 	
 	
 	
@@ -26,18 +53,92 @@ public class MainActivity extends ActionBarActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+    	//----- INITIALIZATION OF SIDEBAR
+	    mTitle = mDrawerTitle = getTitle();
+	    mPlanetTitles = getResources().getStringArray(R.array.menu_array);
+	    mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+	    mDrawerList = (ListView) findViewById(R.id.left_drawer);
+	    
+	    ArrayAdapter<String> adaptador = new ArrayAdapter<String>(getApplicationContext(), R.layout.drawer_list_item, mPlanetTitles);
+	    mDrawerList.setAdapter(adaptador);
+	    mDrawerList.setOnItemClickListener(new DrawerItemClickListener());
+	    
+        getActionBar().setDisplayHomeAsUpEnabled(true);
+        getActionBar().setHomeButtonEnabled(true);
+        mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout, R.drawable.ic_launcher, R.string.drawer_open, R.string.drawer_close)
+		        {
+		        	public void onDrawerClosed(View view)
+		        	{
+		        		super.onDrawerClosed(view);
+		        		getActionBar().setTitle(mTitle);
+		        		invalidateOptionsMenu();
+		        	}
+		        	
+		        	@Override
+		        	public void onDrawerOpened(View drawerView) 
+		        	{
+		        		super.onDrawerOpened(drawerView);
+		        		getActionBar().setTitle(mDrawerTitle);
+		        		invalidateOptionsMenu();
+		        	}
+		        };
+        
+		        mDrawerLayout.setDrawerListener(mDrawerToggle);
+		        if (savedInstanceState == null) {
+		            selectItem(0);
+		        }
+		mDrawerLayout.setDrawerListener(mDrawerToggle);
+		        
         if (savedInstanceState == null) 
         {
-            getSupportFragmentManager().beginTransaction().add(R.id.container, new PlaceholderFragment()).commit();
+            selectItem(0);
         }
-        
-        
-        
-
         
     }
 
 
+    
+    private class DrawerItemClickListener implements ListView.OnItemClickListener
+    {
+		@Override
+		public void onItemClick(AdapterView parent, View view, int position, long id) 
+		{
+			selectItem(position);
+		}
+    	
+    }
+  
+    private void selectItem(int position)
+    {
+    	PlaceholderFragment fragment = new PlaceholderFragment();
+    	Bundle args = new Bundle();
+    	
+//    	fragment.setArguments(args);
+//    	
+//    	FragmentManager fragmentManager = getFragmentManager();
+//    	fragmentManager.beginTransaction().replace(R.id.content_frame, fragment).commit();
+    	
+    	mDrawerList.setItemChecked(position, true);
+    	setTitle(mPlanetTitles[position]);
+    	mDrawerLayout.closeDrawer(mDrawerList);
+    }
+    
+    public void setTitle(CharSequence title)
+    {
+    	mTitle = title;
+    	getActionBar().setTitle(mTitle);
+    }
+    
+    
+    @Override
+    public boolean onPrepareOptionsMenu(Menu menu) 
+    {
+    	boolean drawerOpen = mDrawerLayout.isDrawerOpen(mDrawerList);
+    	menu.findItem(R.id.action_websearch).setVisible(!drawerOpen);
+    	return super.onPrepareOptionsMenu(menu);
+    }
+    
     @Override
     public boolean onCreateOptionsMenu(Menu menu) 
     {
@@ -48,6 +149,11 @@ public class MainActivity extends ActionBarActivity
     @Override
     public boolean onOptionsItemSelected(MenuItem item) 
     {
+    	if(mDrawerToggle.onOptionsItemSelected(item))
+    	{
+    		return true;
+    	}
+    	
         int id = item.getItemId();
         if (id == R.id.action_settings) 
         {
